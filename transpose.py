62b418bd6328262704271c7e3f1e1786fe4d00ed
degD <108623049+degD@users.noreply.github.com>
2023-09-07 17:46:23 +0300

    Made function more readable

--- transpose.py
+++ transpose.py
@@ -5,7 +5,10 @@ def transposeTable(step):
     
     musicalNotes = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"]
     for i, note in enumerate(musicalNotes):
-        print(f"{note.upper()} -> {musicalNotes[(i+int(step))%len(musicalNotes)]}")
+        noteUpper = note.upper()
+        transposeIndex = (i + int(step)) % len(musicalNotes)
+        transposeNote = musicalNotes[transposeIndex]
+        print(f"{noteUpper} -> {transposeNote}")
     
 
 if __name__ == "__main__":
