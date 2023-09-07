3029fc78e772600197b39fbd272baaa0a6a0e12e
degD <108623049+degD@users.noreply.github.com>
2023-09-07 17:42:32 +0300

    Testing for bugs

--- transpose.py
+++ transpose.py
@@ -1,7 +1,13 @@
 
+import sys
+
 def transposeTable(step):
     
     musicalNotes = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"]
     for i, note in enumerate(musicalNotes):
-        print(f"{note.upper()} -> {musicalNotes[(i+step)%len(musicalNotes)]}")
-        
+        print(f"{note.upper()} -> {musicalNotes[(i+int(step))%len(musicalNotes)]}")
+    
+
+if __name__ == "__main__":
+    
+    transposeTable(sys.argv[1])
